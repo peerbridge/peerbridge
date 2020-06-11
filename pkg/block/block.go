ec5165d9bf73c2df82dbd7f907c8d5846706c87a
Felix Kästner <kaestner.fe@gmail.com>
2020-06-11 17:24:24 +0200

    Create initial folder structure - Fix #7

---
+++ pkg/block/block.go
@@ -0,0 +1,23 @@
+package block
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+	"time"
+)
+
+const ISO_8601 = time.RFC3339
+
+type Block struct {
+	Index     uint64
+	Timestamp time.Time
+	PrevHash  string
+}
+
+func (b Block) Hash() string {
+	record := strconv.FormatUint(b.Index, 10) + b.Timestamp.Format(ISO_8601) + b.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	return hex.EncodeToString(h.Sum(nil))
+}
