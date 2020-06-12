e4464fc03112d86b766acb9d77d065edad411121
Felix Kästner <kaestner.fe@gmail.com>
2020-06-12 12:47:11 +0200

    add console color logging

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/peerbridge/peerbridge/pkg/color"
 	"github.com/peerbridge/peerbridge/pkg/http"
 )
 
 func main() {
 	server := http.NewServer()
-	fmt.Println(fmt.Sprintf("Start server listening on: %s", server.Addr))
+	fmt.Println(fmt.Sprintf("Start server listening on: %s", color.Sprintf(server.Addr, color.InfoColor)))
 	log.Fatal(server.ListenAndServe())
 }
