3534f9ee446214d01de4f9c114e881a98d432bc9
Felix Kästner <kaestner.fe@gmail.com>
2020-09-25 19:54:15 +0200

    Fix missing Nonce in Transaction Initializer

--- pkg/blockchain/blockchain_test.go
+++ pkg/blockchain/blockchain_test.go
@@ -18,16 +18,16 @@ func ExampleBlockchainExtension() {
 	message3Data := []byte("You can send anything serializable, really.")
 	message4Data := []byte("I see. Let's use json data with encryption information from now on.")
 
-	transaction1 := Transaction{bobPublicKeyString, alicePublicKeyString, time.Now(), message1Data}
+	transaction1 := Transaction{bobPublicKeyString, alicePublicKeyString, time.Now(), message1Data, Nonce()}
 	blockChain.AddTransaction(transaction1)
-	transaction2 := Transaction{alicePublicKeyString, bobPublicKeyString, time.Now(), message2Data}
+	transaction2 := Transaction{alicePublicKeyString, bobPublicKeyString, time.Now(), message2Data, Nonce()}
 	blockChain.AddTransaction(transaction2)
 
 	blockChain.ForgeNewBlock()
 	fmt.Println("Forged the first block. This many transactions are to be found for Alice:")
 	fmt.Println(len(blockChain.GetForgedTransactions(alicePublicKeyString)))
 
-	transaction3 := Transaction{bobPublicKeyString, alicePublicKeyString, time.Now(), message3Data}
+	transaction3 := Transaction{bobPublicKeyString, alicePublicKeyString, time.Now(), message3Data, Nonce()}
 	blockChain.AddTransaction(transaction3)
 
 	blockChain.ForgeNewBlock()
@@ -38,7 +38,7 @@ func ExampleBlockchainExtension() {
 	fmt.Println("Forged the third block. This many transactions are to be found for Alice:")
 	fmt.Println(len(blockChain.GetForgedTransactions(alicePublicKeyString)))
 
-	transaction4 := Transaction{alicePublicKeyString, bobPublicKeyString, time.Now(), message4Data}
+	transaction4 := Transaction{alicePublicKeyString, bobPublicKeyString, time.Now(), message4Data, Nonce()}
 	blockChain.AddTransaction(transaction4)
 
 	blockChain.ForgeNewBlock()
--- pkg/blockchain/transaction.go
+++ pkg/blockchain/transaction.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"crypto/rand"
 	"crypto/rsa"
 	"time"
 
@@ -10,11 +11,19 @@ import (
 type PEMPublicKey = string
 
 type Transaction struct {
-	Nonce     []byte       `json:"nonce"`
 	Sender    PEMPublicKey `json:"sender"`
 	Receiver  PEMPublicKey `json:"receiver"`
 	Timestamp time.Time    `json:"timestamp"`
 	Data      []byte       `json:"data"`
+	Nonce     []byte       `json:"nonce"`
+}
+
+func Nonce() (nonce []byte) {
+	nonce = make([]byte, 12)
+	if _, err := rand.Read(nonce); err != nil {
+		panic(err.Error())
+	}
+	return
 }
 
 func (t Transaction) SenderPublicKey() (*rsa.PublicKey, error) {
