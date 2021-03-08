e6be839d7e09966cf7e695092674ddd7b1c4079c
PhilippMatthes <github@philippmatth.es>
2021-03-08 15:02:28 +0100

    Add simple peer url route and include p2p into main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,30 +11,41 @@ import (
 	"github.com/peerbridge/peerbridge/pkg/color"
 	"github.com/peerbridge/peerbridge/pkg/database"
 	. "github.com/peerbridge/peerbridge/pkg/http"
+	"github.com/peerbridge/peerbridge/pkg/peer"
 )
 
 const blockCreationInterval = 3
 
 func main() {
+	bootstrapTarget := flag.
+		String("bootstrap", "", "The bootstrap target url")
+	flag.Parse()
+
+	// Initialize the database models
 	models := []interface{}{
 		(*blockchain.Block)(nil),
 		(*blockchain.Transaction)(nil),
 	}
-
 	err := database.Initialize(models)
 	if err != nil {
 		panic(err)
 	}
 
+	// Run the p2p peer server concurrently
+	go peer.Run(bootstrapTarget)
+
+	// Schedule the periodic block creation
 	ticker := time.NewTicker(blockCreationInterval * time.Second)
 	go blockchain.ScheduleBlockCreation(ticker)
 
+	// Create a http router and start serving http requests
 	router := NewRouter()
 	router.Use(Header, Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to PeerBridge!"))
 	})
 	router.Mount("/blockchain", blockchain.Routes())
+	router.Mount("/peer", peer.Routes())
 
 	fmt.Println(fmt.Sprintf("Start server listening on: %s", color.Sprintf(GetServerPort(), color.Info)))
 	log.Fatal(router.ListenAndServe())
